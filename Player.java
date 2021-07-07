78101b5d0e8c9241cd3a8175cf3b731b4fb3cde5
ChathumiFernando <nanediri.20200948@iit.ac.lk>
2021-07-07 12:36:20 +0530

    First Change

---
+++ Player.java
@@ -0,0 +1,28 @@
+package MyJava;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Player {
+    String Name;
+    int Age;
+    String Type;
+    int statistic;
+
+    public void getAverage(){
+
+    }
+
+    public void getDetails(){
+
+    }
+
+    public void getPerformence(){
+
+    }
+
+    public void getType(){
+
+    }
+
+}
