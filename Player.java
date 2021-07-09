37f93322f6240f3c01aa995b8208646aef695e3d
ChathumiFernando <87010203+ChathumiFernando@users.noreply.github.com>
2021-07-09 08:59:38 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,24 +4,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Player {
-    String Name;
-    int Age;
-    String Type;
-    int statistic;
+    public String playerName;
+    public int playerAge;
+    public String playerType;
+    public int playerStatistic;
+
+    public Player(){
+
+    }
+
+    public Player(String playerName,int playerAge, String playerType, int playerStatistic){
+        this.playerName = playerName;
+        this.playerAge = playerAge;
+        this.playerType = playerType;
+        this.playerStatistic = playerStatistic;
+    }
 
     public void getAverage(){
-
+        float average=playerStatistic/15;
     }
 
     public void getDetails(){
-
+        System.out.println(playerName+" "+playerAge+" "+playerType+" "+playerStatistic+" " );
     }
 
-    public void getPerformence(){
-
+    public void getBestPerformence(){
+        System.out.println();
     }
 
     public void getType(){
+        System.out.println();
+    }
+
+    public void addPlayer(String a){
 
     }
 
